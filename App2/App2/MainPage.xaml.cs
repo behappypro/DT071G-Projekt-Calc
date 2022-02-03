32493eb614035ec2e47e89784200d447b515d6c6
behappypro <39382867+behappypro@users.noreply.github.com>
2022-02-03 22:58:12 +0100

    Added comments

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -21,11 +21,14 @@ namespace App2
             InitializeComponent();
         }
         
+        // Funktion för att lägga till nummer i uttrycket
         private void selectNumber(object sender, EventArgs e)
         {
             Button button = (Button)sender;
+            // Om uttrycket är nollställd tömts det
             if (txtExpression.Text == "0") txtExpression.Text = "";
             
+            // Om vi har gjort en kalkylation tidigare så tömmer vi uttrycket och börjar på nytt
             if (calculate == true)
             {
                 txtExpression.Text = "";
@@ -35,6 +38,7 @@ namespace App2
 
             else
             {
+                // Om sista tecknet är en parentes och vi lägger till ett nummer, sätt gongertecken före
                 if (txtExpression.Text.Length > 0 && txtExpression.Text.ElementAt(txtExpression.Text.Length - 1) == ')')
                 {
                     txtExpression.Text += "×";
@@ -47,18 +51,20 @@ namespace App2
             blockOperator = false;
         }
         
+        // Funktion för att välja operatör
         private void selectOperator(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             int operatorCounter = 0;
             Operator = false;
 
+            // Om operatör redan finns i uttrycket och vi skriver ny operatör så ersätts den
             if (Operator == true && button.Text != "-" && button.Text != "(")
             {
                 txtExpression.Text = txtExpression.Text.Remove(txtExpression.Text.Length - 1, 1);
                 txtExpression.Text += button.Text;
             }
-
+            // Om uttrycket är felaktigt och vi lägger till operatör så nollställs den
             if (txtExpression.Text == "Felaktigt uttryck")
             {
                 txtExpression.Text = "0";
@@ -70,6 +76,7 @@ namespace App2
                 return;
             }
 
+            // Om vi skriver en punkt och sista tecknet inte är en siffta så läggs noll innan
             if (button.Text == ".")
             {
                 if (!Char.IsDigit(txtExpression.Text.ElementAt(txtExpression.Text.Length - 1)) &&
@@ -81,22 +88,24 @@ namespace App2
                 Decimal = true;
             }
 
-
+            // Om knappen är en parentes eller minustecken eller boolean är false
             if (button.Text == "(" || button.Text == "-" || Operator == false|| Decimal == false)
             {
             
+                // Ät uttrycket nollställt?
                 if (txtExpression.Text == "0"){
 
+                    // Ät knappen en parentes eller roten ur, ersätt med värdet
                     if (button.Text == "(" || button.Text == "√")
                     {
                         txtExpression.Text = button.Text;
                     }
-
+                    // Annars lägg till värde på befintligt
                     else
                     {
                         txtExpression.Text += button.Text;
                     }
-
+                    // Är knappen slutparentes och uttrycket är noll, Lämna det noll 
                     if (button.Text == ")")
                     {
                         txtExpression.Text = "0";
@@ -106,18 +115,21 @@ namespace App2
                 else if (button.Text == "(")
                 {
                   
+                    // Loopar igenom alla operatörer
                     for (int i = 0; i < operators.Length; i++)
                     {
+                        // Är sista tecknet en operatör, ökar räknaren med ett
                         if (txtExpression.Text.ElementAt(txtExpression.Text.Length - 1) == operators[i])
                         {
                             operatorCounter++;
                         }
                     }
-
+                    // Är räknaren större än 1 läggs värdet till
                     if (operatorCounter > 0)
                     {
                         txtExpression.Text += button.Text;
                     }
+                    // Annars läggs nedanstående värde
                     else txtExpression.Text += "×(";
                 }
 
@@ -129,18 +141,19 @@ namespace App2
                 }
 
                 else{
-
+                    // Om det är slutparentes sätts boolean till false
                     if (button.Text == ")")
                     {
                         Operator = false;
                         blockOperator = false;
                     }
+                    // Annars sätter vi de till true så att vi inte kan skriva dubbel operatör
                     else
                     {
                         Operator = true;
                         Decimal = true;
                     }
-
+                    // Lägger till knappens operatör till uttrycket
                     blockOperator = false;
                     txtExpression.Text += button.Text;
                     
@@ -149,23 +162,28 @@ namespace App2
             calculate = false;
             
         }
-
+        // Funktion för att rensa hela uttrycket
         private void btnAllClear_Clicked(object sender, EventArgs e)
         {
+            // Återställer texten och booleans till false
             txtExpression.Text = "0";
             Operator = false;
             blockOperator = false;
 
         }
 
+        // Funktion för att radera senaste tecknet
         private void btnClear_Clicked(object sender, EventArgs e)
         {
             Button button = (Button)sender;
+            // Hämtar senaste tecknet
             char lastElement = txtExpression.Text.ElementAt(txtExpression.Text.Length - 1);
 
             if (txtExpression.Text == "0") return;
+            // Raderar sista tecknet
             txtExpression.Text = txtExpression.Text.Remove(txtExpression.Text.Length - 1, 1);
 
+            // Om tecknet är en operatör så sätts booleans till false
             if (lastElement == '+'|| lastElement == '×' || lastElement == '÷' || lastElement == '-')
             {
                 Operator = false;
@@ -173,15 +191,18 @@ namespace App2
             }
             else Operator = true;
 
+            // Är fältet tomt så nollställs det
             if (string.IsNullOrEmpty(txtExpression.Text)) txtExpression.Text = "0";
         }
 
-
+        // Funktion för att räkna ut ett uttryck
         private void btnCalculate_Clicked(object sender, EventArgs e)
         {
             calculate = true;
+            // Hämtar värdet av uttrycket
             string expression = txtExpression.Text;
             currentNumber.Text = expression;
+            // Om uttrycket är felaktikt och använder trycker på = så nollställs det
             if (expression == "Felaktigt uttryck")
             {
                 txtExpression.Text = "0";
@@ -189,12 +210,15 @@ namespace App2
             }
             else
             {
+                // Skickar vårat uttryck som argument till klassen och funktionen
                 double result = Calculator.Calculate(expression);
 
+                // Är uttrycket inte ett nummer, då skrivs felmeddelande ut
                 if (double.IsNaN(result))
                 {
                     txtExpression.Text = "Felaktigt uttryck";
                 }
+                // Ananrs skrivs svaret ut
                 else
                 {
                     txtExpression.Text = result.ToString();
